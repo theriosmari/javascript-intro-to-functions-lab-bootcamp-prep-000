83908c681e392024887dcb9c88ceec5725d7a90c
Mariel Ríos <marielbrios09@gmail.com>
2017-12-20 00:33:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,16 +11,22 @@ function logShout(string) {
 }
 
 function sayHiToGrandma(string) {
-  string === string.toLowerCase()
+  if (string === string.toLowerCase()) {
     return 'I can\'t hear you!'
+  } else if (string === string.toUpperCase()) {
+      return 'YES INDEED!'
+    } else if (string === 'I love you, Grandma.') {
+      return 'I love you, too.'
+      }
+  }
+
+  
+    
+  
+  
+}
+    
 }
 
-function sayHiToGrandma(string) {
-  string === string.toUpperCase()
-    return 'YES INDEED!'
-}
-
-function sayHiToGrandma(string) {
-  string === 'I love you, Grandma.'
-    return 'I love you, too.'
+  
 }
