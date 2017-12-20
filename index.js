16fafd8ddb09573c0f3b14e725c13562e55500d6
Mariel Ríos <marielbrios09@gmail.com>
2017-12-20 00:38:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logShout(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return 'I can\'t hear you!'
-  } else if (string === string.toUpperCase()) {
+  } else if (string.toUpperCase() === string) {
       return 'YES INDEED!'
     } else if (string === 'I love you, Grandma.') {
       return 'I love you, too.'
