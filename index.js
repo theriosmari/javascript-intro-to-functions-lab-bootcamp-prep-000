69759ee5c15869a887679fb2288cca2c1a531cb9
Mariel Ríos <marielbrios09@gmail.com>
2017-12-20 00:40:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+function shout(string) {
+  return string.toUpperCase()
+}
 
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase);
+}
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
